735e95a1cffeb87ff39208a8a0c54e121b6b6bb9
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2022-05-12 21:18:47 -0300

    refatorando script e adicionando imagem descritiva

--- desafio.js
+++ desafio.js
@@ -1,7 +1,6 @@
 let prompt = require("prompt-sync")();
 
 let str1 = "";
-let str2 = "";
 
 const T = prompt("Qual o tamanho da ampulheta? ");
 
@@ -28,23 +27,4 @@ for (let i = 0; i < T; i++) {
   str1 += "\n";
 }
 
-for (let i = 0; i < T; i++) {
-  for (let j = 0; j < T; j++) {
-    if (
-      (j <= i && j >= T - i) ||
-      j == T - 1 ||
-      j == 0 ||
-      i + j == T - 1 ||
-      i - j == 0 ||
-      i == 0
-    ) {
-      str2 += "#";
-    } else {
-      str2 += " ";
-    }
-  }
-  str2 += "\n";
-}
-
 console.log(str1);
-console.log(str2);
